df1739331e491c9c357109d5979c8f3c03b7556c
Saurabh Kumar <saurabhkumar4829@gmail.com>
2021-11-28 16:22:59 +0530

    Update miscellaneous.py

--- miscellaneous.py
+++ miscellaneous.py
@@ -28,8 +28,20 @@ class Stack():
 
     def pop(self):
         return self.items.pop()
-
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        
     def get_stack(self):
         return self.items
 
 myStack = Stack()
+myStack.push("A")
+myStack.push("B")
+myStack.push("C")
+myStack.push("D")
+print(myStack.peek())
