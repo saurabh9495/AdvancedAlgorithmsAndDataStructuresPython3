cc2b1c6778f9b577ecdf528793c6a219f4d38fa0
Saurabh Kumar <saurabhkumar4829@gmail.com>
2021-11-28 17:42:57 +0530

    Update miscellaneous.py

--- miscellaneous.py
+++ miscellaneous.py
@@ -46,6 +46,31 @@ myStack.push("C")
 myStack.push("D")
 print(myStack.peek())
 
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+
+    def size(self):
+        return len(self.items)
+
+
 class Node(object):
     def __init__(self, value):
         self.value = value
@@ -64,6 +89,8 @@ class BinaryTree(object):
             return self.inorder_print(tree.root, "")
         elif traversal_type == "postorder":
             return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.levelorder_print(tree.root)
 
         else:
             print("Traversal type " + str(traversal_type) + " is not supported.")
@@ -93,3 +120,30 @@ class BinaryTree(object):
             traversal += (str(start.value) + "-")
         return traversal
 
+    def levelorder_print(self, start):
+        if start is None:
+            return 
+
+        queue = Queue()
+        queue.enqueue(start)
+
+        traversal = ""
+        while len(queue) > 0:
+            traversal += str(queue.peek()) + "-"
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+
+        return traversal
+
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+
+print(tree.print_tree("levelorder"))
