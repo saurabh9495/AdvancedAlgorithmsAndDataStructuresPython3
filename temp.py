07c999a3f03d5a495b88ae4b1bb1f91f5608890f
Saurabh Kumar <saurabhkumar@Saurabhs-MacBook-Air.local>
2022-04-13 07:20:22 +0530

    ok

--- temp.py
+++ temp.py
@@ -1,34 +1,30 @@
-from Node import Node
+from DoublyLinkedList import DoublyLinkedList
 
-class LinkedList:
+class MyQueue:
     def __init__(self):
-        self.head_node = None
-    def insert_at_head(self,data):
-        temp_node = Node(data)
-        temp_node.next_element = self.head_node
-        self.head_node = temp_node
-        return self.head_node
+        self.items = DoublyLinkedList()
+
     def is_empty(self):
-        if self.head_node == None:
-            return True
-        else:
-            return False
-    def print_list(self):
+        return self.items.length == 0
+
+    def front(self):
         if self.is_empty():
-            print("List is empty")
-            return False
-        temp = self.head_node
-        while temp.next_element is not None:
-            print(temp.data, end=" -> ")
-            temp = temp.next_element
-        print(temp.data, "-> None")
-        return True
+            return None
+        return self.items.get_head()
+
+    def rear(self):
+        if self.is_empty():
+            return None
+        return self.items.tail_node()
+
+    def size(self):
+        return self.items.length
 
 
-list = LinkedList()
-list.print_list()
+if __name__ == "__main__" :
+    queue_obj = MyQueue()
 
-print("Inserting values in the list")
-for i in range(1,10):
-    list.insert_at_head(i)
-print(list.print_list())
+    print("is_empty(): " + str(queue_obj.is_empty()))
+    print("rear(): " + str(queue_obj.rear()))
+    print("front(): " + str(queue_obj.front()))
+    print("size(): " + str(queue_obj.size()))
