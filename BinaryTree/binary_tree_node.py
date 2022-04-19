4435bf5718547e53ec6b4e8e66c9e2a093a8959a
Saurabh Kumar <saurabhkumar@Saurabhs-MacBook-Air.local>
2022-04-19 11:40:57 +0530

    29thApril

---
+++ BinaryTree/binary_tree_node.py
@@ -0,0 +1,9 @@
+class BinaryTreeNode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+        self.next = None
+        self.parent = None
+        self.count = None
